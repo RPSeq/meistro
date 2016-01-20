13915f58a0885d4f9e3688a967aacb0d3531ee20
SmithRyan <ryan.smith.p@gmail.com>
2016-01-20 17:46:28 -0600

    Updated filtering. Still incomplete.

--- filter_merged.py
+++ filter_merged.py
@@ -21,7 +21,7 @@ __date__ = "$Date: 2014-12-15 11:43 $"
 # functions
 # ============================================
 
-def filter_merged(bamfile, is_sam, out_bam):
+def filter_merged(bamfile, is_sam, out_bam, mei_names):
     """Main BAM parsing loop"""
 
     # set input file
@@ -41,26 +41,34 @@ def filter_merged(bamfile, is_sam, out_bam):
     # header = "@HD\tVN:1.3\tSO:queryname\n"
     # header+="\n".join(in_bam.text.split("\n")[1:])
 
+    #get meinames from file
+    meis = set(line.strip() for line in mei_names)
+
+
     #assuming name-sorted bam
     filtered = []
-    group = Namegroup(in_bam)
+    group = Namegroup(in_bam, meis)
 
-    sys.stderr.write("Filtering...\n")
+
+
+    #sys.stderr.write("Filtering...\n")
     for al in in_bam:
+        if al.rname < 0:
+            continue
         #if qname doesnt match current group:
         if not group.add(al):
             #pass the group to filtering function
             hit = group.process()
             #if the group hit an MEI, append to filtered list.
             if hit:
-                filtered.extend(group.als)
+                filtered.extend(group.anchors)
             #create a new namegroup
-            group = Namegroup(in_bam, al)
+            group = Namegroup(in_bam, meis, al)
 
     for al in filtered:
         out_bam.write(al)
 
-    sys.stderr.write("Done!\n")
+    #sys.stderr.write("Done!\n")
 
 def mismatch(al):
     """Returns number of mismatches in given alignment"""
@@ -82,6 +90,7 @@ description: Group BAM file by read IDs without sorting")
     parser.add_argument('-i', '--input', metavar='BAM', required=False, help='Input BAM file')
     parser.add_argument('-S', required=False, action='store_true', help='Input is SAM format')
     parser.add_argument('-o', required=True, help='Output MEI BAM')
+    parser.add_argument('-m', '--mei_names', required=True, type=argparse.FileType('r'), help='List of MEI reference names')
     # parse the arguments
     args = parser.parse_args()
     
@@ -101,10 +110,13 @@ description: Group BAM file by read IDs without sorting")
 class Namegroup(object):
     """Class for containing reads of the same name"""
 
-    def __init__(self, in_bam, al=False):
+    def __init__(self, in_bam, meis, al=False):
 
         self.bam = in_bam
         self.als = []
+        self.anchors = []
+        self.mei = False
+        self.mei_rnames = meis
 
         if al:
             self.name = al.qname.split("_")[0]
@@ -114,9 +126,13 @@ class Namegroup(object):
             self.name = False
 
     def add(self,al):
+        name, num = al.qname.split("_")
+        tags = al.opt("TY").split(",")
+        #if self.name hasn't been set, set with current al.
         if self.name == False:
-            self.name = al.qname.split("_")[0]
-        elif al.qname.split("_")[0] != self.name:
+            self.name = name
+        #if the name doesn't match the group, return false
+        if name != self.name:
             return False
 
         self.als.append(al)
@@ -125,103 +141,72 @@ class Namegroup(object):
     def process(self):
         """Returns true if group hit an MEI"""
 
-        #Tags used for characterizing an MEI namegroup.
-        #If an alignment in the group matches a tag, the tag is set to that alignment.
-        #MUU and AUU are MEI Unique-Unique and Anchor Unique-Unique
-        MUU1,MUU2,AUU1,AUU2=False,False,False,False
-        #UR and RU are Repeat-Unique and Unique-Repeat
-        UR1,UR2,RU1,RU2=False,False,False,False
-        #ASL and ASR are (Anchor, Split Left) and (Anchor, Split Right)
-        ASL1,ASL2,ASR1,ASR2=False,False,False,False
-        #SL and SR are Split Left and Split Right
-        SL1,SL2,SR1,SR2=False,False,False,False
-
-
         for al in self.als:
             #get name and read #
             name, num = al.qname.split("_")
             tags = al.opt("TY").split(",")
+            #some untig als have non-spec rname indices
             if al.rname < 0:
                 continue
             if not al.is_secondary: #ignore secondary hits for now
-
-                # Here we update all the tag info
-                if "UU" in tags:
-                    if "moblist" in self.bam.getrname(al.rname):
-                        if num == "1":
-                            MUU1 = al
-                        elif num == "2":
-                            MUU2 = al
+                #check for both UUs aligned to MEI
+                if 'UU' in tags:
+                    #if its an MEI alignment, and another doesn't exist,
+                    #keep it.
+                    if self.bam.getrname(al.rname) in self.mei_rnames:
+                        if not self.mei:
+                            self.mei = al
+                        #if we've already seen an mei UU alignment,
+                        #both sides of the UU pair have mapped to the MEI.
+                        else:
+                            return False
                     else:
-                        if num == "1":
-                            AUU1 = al
-                        elif num == "2":
-                            AUU2 = al
+                        self.anchors.append(al)
 
-                if "UR" in tags:
-                    if num == "1":
-                        UR1 = al
-                    elif num == "2":
-                        UR2 = al
+                elif any(x in tags for x in ['SL','SR','RU']):
+                    self.mei = al
+                #check for anchor aligned tags
+                elif any(x in tags for x in ['ASL','ASR','UR']):
+                    self.anchors.append(al)
 
-                if "RU" in tags:
-                    if num == "1":
-                        RU1 = al
-                    elif num == "2":
-                        RU2 = al
+        # check for anchor ref chrom discrepancies 
+        # if there are multiple anchor reads in the group.
+        #     discard if there are mismatches.
+        if len(self.anchors) > 1:
+            rname = self.anchors[0].rname
+            for anch in self.anchors[1:]:
+                if anch.rname != rname:
+                    return False
 
-                if "ASL" in tags:
-                    if num == "1":
-                        ASL1 = al
-                    elif num == "2":
-                        ASL2 = al
+        #some (known) cases left to account for:
+        # UU/UU-Split.
+        #       First UU isn't informative, only the split realignment matters.
+        #       Should probably remove the UU-only anchor (leaving only the split anchor)
+        # UU-anchor/UU-mei:
+        #       This one is probably fine for now, may want to relabel though.
+        # RU/UR-split:
 
-                if "ASR" in tags:
-                    if num == "1":
-                        ASR1 = al
-                    elif num == "2":
-                        ASR2 = al
-
-                if "SL" in tags:
-                    if num == "1":
-                        SL1 = al
-                    elif num == "2":
-                        SL2 == al
-
-                if "SR" in tags:
-                    if num == "1":
-                        SR1 = al
-                    elif num == "2":
-                        SR2 = al
-
-
-        # ==================================== #
-        # main logic for filtering mei groups! #
-        # ==================================== #
-
-        #If both sides of a UU pair map to MEI, remove.
-        if MUU1 and MUU2:
-            #UNLESS 
-            # if (SR1 and SL2) or (SR2 and SL1):
-            #     return True
-            return False
-
-        #If UR pair, keep
-        if (UR1 and RU2) or (UR2 and RU1):
+        #if we have anchor(s) and an mei, time to add the mei info to the anchors.
+        if len(self.anchors) > 0 and self.mei:
+            #if we have mei and anchor, return true.
+            for anc in self.anchors:
+                tags = anc.opt("TY").split(",")
+                mei_name = self.bam.getrname(self.mei.rname)
+                mei_pos = self.mei.pos
+                mei_cigar = self.mei.cigarstring
+                mei_qual = self.mei.mapq
+                mei_tags = ";".join(self.mei.opt("TY").split(","))
+                if self.mei.is_reverse:
+                    mei_ori = "-"
+                else:
+                    mei_ori = "+"
+                anc.setTag("ME", ",".join([mei_name,mei_tags,str(mei_pos),mei_ori,mei_cigar,str(mei_qual)]))
+                #anc.rnext = self.mei.rname
+                #anc.mpos = self.mei.pos
+                anc.tlen = 0
             return True
 
-        #split left, keep
-        if (SL1 and ASL1) or (SL2 and ASL2):
-            return True
-
-        #split right, keep
-        if (SR1 and ASR1) or (SR2 and ASR2):
-            return True
-
-
-
-
-        
+        return False
 
 # ============================================
 # driver
@@ -233,7 +218,7 @@ class Usage(Exception):
 
 def main():
     args = get_args()
-    filter_merged(args.input, args.S, args.o)
+    filter_merged(args.input, args.S, args.o, args.mei_names)
 
 if __name__ == "__main__":
     try:
