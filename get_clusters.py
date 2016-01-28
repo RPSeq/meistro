7650926bd35c34dab3b64345ca1ce074c4621313
SmithRyan <ryan.smith.p@gmail.com>
2016-01-28 16:21:49 -0600

    Extremely simple script for getting clusters with >= 2 hits.

---
+++ get_clusters.py
@@ -0,0 +1,20 @@
+from collections import OrderedDict
+
+infile = open("/dev/stdin", 'r')
+outfile = open("/dev/stdout", 'w')
+
+hits = OrderedDict()
+for line in infile:
+    id = line.strip().split("\t")[8]
+    if id not in hits:
+        hits[id] = [line]
+    else:
+        hits[id].append(line)
+
+for id in hits:
+    if len(hits[id]) > 2:
+        for item in hits[id]:
+            outfile.write(item)
+
+infile.close()
+outfile.close()
