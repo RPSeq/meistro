d43a0b022cbb3011f0dc0e362b5f8dc82c169791
SmithRyan <ryan.smith.p@gmail.com>
2016-04-21 14:18:09 -0500

    Now have working Cluster, SubCluster and MergedCluster objects

--- cluster.py
+++ cluster.py
@@ -7,105 +7,88 @@ __author__ = "Ryan Smith (ryanpsmith@wustl.edu)"
 __version__ = "$Revision: 0.0.1 $"
 __date__ = "$Date: 2016-2-8 13:45 $"
 
-
-# we will have one active cluster for each family (as we see the reads)
-# scanning across. als are collected into their appropriate sub_cluster.
-# when cluster is finished collecting, need to merge subclusters (can be PR,PL,SR,SL (and polyA?))
-
-# clust type can be pair or split. but, i have a generic L or R orientation for splits
-# and do not have the same for pairs; however there is no reason i can't.
-# so, here i define a new convention: + paired anchors are R, as the MEI segment is on the RIGHT from the anchor segment.
+# here i define a new convention: + paired anchors are R, as the MEI segment is on the RIGHT from the anchor segment.
 # and thus, - paired anchors are L, as the MEI segment is on the LEFT relative to the anchor.
 # now, as long as the orientations for a cluster are concordant, (+-,++) are PR and (-+, --) are PL.
-# it might be a good idea to modify filter_merged.py to change RU and UU to PR and PL.
-# BUT: SR and SL do not have a native anchor orientation; that is, a PL can still be in the positive orientation,
-# but the 5' side of the segment is clipped and remapped to an MEI. how do i account for this in the generic sub_cluster type?
 
 class Cluster(object):
-    def __init__(self, cluster=False):
-        #mei_family, anchor_type, orientation : anchors
-        self.hash = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+    def __init__(self, cluster):
+        #mei_family, 
+        #   anchor_type,
+        #       orientation : anchor_list
+        self.collector = defaultdict(lambda: 
+                            defaultdict(lambda: 
+                                defaultdict(list)))
+
         self.ev_count = 0
         self.polyA_count = 0
         self.mei_count = 0
 
+        #map concordant split oris to same value to simplify collection
         self.split_conc_oris = {"+-":"+--+", "-+":"+--+", 
                                 "++":"++--","--":"++--"}
 
-        #load the anchors into the hash
-        if cluster: 
-            self.load(cluster)
-            self.generate_sub_clusters()
+        #collect the anchors into the hash
+        self.collect(cluster)
 
+        #group them into subclusters
+        self.load_sub_clusters()
 
-    def load(self, cluster):
+    def collect(self, cluster):
         for anch in cluster:
+
             for tag in anch.tags:
                 self.ev_count += 1
+
                 if tag.mei == "polyA":
                     self.polyA_count += 1
+
                 else:
                     self.mei_count += 1
+
                 ori = tag.ori
+
                 if tag.RA_type[0] == "S":
                     ori = self.split_conc_oris[ori]
 
                 #for now, the mei_refname's first two chars are the mei_family hash keys
-                self.hash[tag.mei[:2]][tag.RA_type][ori].append(anch)
+                self.collector[tag.mei[:2]][tag.RA_type][ori].append(anch)
 
+    def load_sub_clusters(self):
+        #traverse the collector, generating subclusters as we go
+        #need to load this info into better data structure now
+        for mei_fam, sides in self.collector.iteritems():
 
-    def generate_sub_clusters(self):
-        for mei_fam, sides in self.hash.iteritems():
             for side, oris in sides.iteritems():
+
                 for ori, anchs in oris.iteritems():
-                    self.hash[mei_fam][side][ori] = self.sub_cluster(anchs, side)
+                    self.collector[mei_fam][side][ori] = self.gen_subclusters(anchs, side, ori)
 
 
-    def filter(self):
-        """Define filter for clusters"""
-        if self.mei_count > 2:
-            return True
-        return False
 
+    def gen_subclusters(self, clust, side, ori):
+        """Generate subclusters from a list of anchors"""
 
-    def sub_cluster(self, anch_list, side):
+        #sort anchors by end pos if MEI on right.
+        if side[1] == "R":
+            #sort anchors by end position
+            clust.sort(key=lambda x: x.end)
 
-        if not anch_list: return False
-
-        read_side = side[1]
-        read_type = side[0]
-        #if MEI on right side: 
-        if read_side == "R":
-            #re-sort anchors by end position
-            anch_list.sort(key=lambda x: x.end)
-
-        #set appropriate distance param
-        if read_type == "S":
-            dist = SPLIT_CLUST_DIST
-
-        elif read_type == "P":
-            dist = PAIR_CLUST_DIST
-
-        return self.subclust_gen(anch_list, dist, read_side)
-
-    def subclust_gen(self, clust, distance, side):
         clusters = []
         prev = None
 
         for anch in clust:
+
             if not prev:
                 prev = anch
-                curr_clust = [prev]
+                curr_clust = SubCluster(prev, side, ori)
                 continue
 
             curr = anch
 
-            if self.outside(curr, prev, distance, side):
+            if not curr_clust.add(prev, side):
                 clusters.append(curr_clust)
-                curr_clust = [curr]
-
-            else:
-                curr_clust.append(curr)
+                curr_clust = SubCluster(curr, side, ori)
 
             prev = curr
 
@@ -113,46 +96,134 @@ class Cluster(object):
 
         return clusters
 
-    def outside(self, curr, prev, distance, side):
-        if side == "L":
-            if curr.start - prev.start > distance:
-                return True
 
-        if side == "R":
-            if curr.end - prev.end > distance:
-                return True
+    def filter(self):
+        """Define filter for clusters"""
+        if self.mei_count > 10:
+
+            return True
 
         return False
 
+class MergedCluster(object):
+    def __init__(self, ClusterA, ClusterB, dist):
 
-    def __str__(self, id_num=False):
-        outstr = ""
-        clust_id = 0
-        for mei_fam, sides in self.hash.iteritems():
-            for side, oris in sides.iteritems():
-                for ori, clusters in oris.iteritems():
-                    for clust in clusters:
-                        for anch in clust:
-                            for tag in anch.tags:
-                                if tag.RA_type == side:
-                                    mei = tag.mei
-                                    m_cigar = tag.cigar
-                                    m_ori = tag.ori
-                            outstr+="\t".join([anch.chrom, 
-                                            str(anch.start), 
-                                            str(anch.end), 
-                                            str(anch.mapq), 
-                                            anch.name, 
-                                            mei_fam, 
-                                            mei, 
-                                            m_ori, 
-                                            side, 
-                                            anch.cigar, 
-                                            m_cigar, 
-                                            "clust_"+str(clust_id), 
-                                            str(id_num)])+"\n"
-                        clust_id+=1
-        return outstr
+        if ClusterA.dir == ClusterB.dir or ClusterA.dir:
+            sys.stderr.write("Error: Clusters to merge have same side (L/R")
+            exit(1)
+
+        side = ClusterA.side
+        if side == "L":
+            self.L_Cluster = Cluster.A
+            self.R_Cluster = Cluster.B
+
+        elif side == "R":
+            self.R_Cluster = Cluster.A
+            self.L_Cluster = Cluster.B
+
+        self.r_type == ClusterA.r_type
+        self.dist = dist
+
+    # def get_tsd(self)
+
+class SubCluster(object):
+    def __init__(self, Cluster, anchor, side, ori):
+        self.cluster = Cluster
+        self.anchors = [anchor]
+        self.side = side
+        self.read_type = side[0]
+        self.dir = side[1]
+        self.ori = ori
+
+        pos = anchor.start
+        if self.dir == "R":
+            pos = anchor.end
+
+        self.pos = pos
+        self.start = pos
+        self.end = pos
+
+        if self.r_type == "S":
+            self.clust_dist = SPLIT_MERGE_DIST
+            self.merge_dist = SPLIT_MERGE_OLAP
+            self.merge_olap = SPLIT_CLUST_DIST
+
+        elif self.r_type == "P":
+            self.clust_dist = PAIR_MERGE_DIST
+            self.merge_dist = PAIR_MERGE_OLAP
+            self.merge_olap = PAIR_CLUST_DIS
+
+    def add(self, anchor, side):
+        if self.side != side:
+            return False
+
+        a_pos = self.get_a_pos(anchor)
+
+        if a_pos - self.end > self.clust_dist:
+            return False
+
+        else:
+            self.anchors.append(anchor)
+            self.end = a_pos
+            return True
+
+    def get_a_pos(self, anchor):
+
+        a_pos = anchor.end
+        if self.side == "L":
+            a_pos = anchor.start
+
+        return a_pos
+
+    # def get_ori(self):
+    #     anch = self.anchors[0]
+    #     for tag in anch.tags:
+    #         if tag.RA_type == self.side:
+    #             return tag.ori
+
+    def clust_pos(self):
+        """Returns cluster coord from appropriate R or L side"""
+
+        if self.dir == "R":   return self.end
+
+        elif self.dir == "L": return self.start
+
+    def clust_dist(self, cluster):
+        """Returns the distance between two SubClusters"""
+
+        if self.dir == "R":
+            return cluster.clust_pos() - self.clust_pos
+
+        elif self.dir == "L":
+            return self.clust_pos - cluster.clust_pos()
+
+    def merge(self, cluster):
+        """Returns False, False if discordant, 
+        False, dist if too far/excess olap,
+        and True, MergedCluster object if can be merged."""
+
+        #not concordant for merging
+        if self.dir == cluster.dir or self.read_type != cluster.read_type:
+            return False, False
+
+        #get distance between inside ends
+        dist = self.clust_dist(cluster)
+
+        #get split/pair cutoff values
+        m_olap, m_dist = PAIR_MERGE_OLAP, PAIR_MERGE_DIST
+        if self.read_type == "P":
+            m_olap, m_dist = SPLIT_MERGE_OLAP, SPLIT_MERGE_DIST
+
+        #too much overlap or too far:
+        if dist < (0-m_olap) or dist > m_dist:
+            return False, dist
+
+        else:
+            return True, MergedCluster(self, cluster, dist)
+
+
+
+
 
 class meiTag(object):
     """Encapsulates an mei realignment tag"""
@@ -209,9 +280,10 @@ def scan(bamfile, is_sam):
     for group in cluster_generator(in_bam, PRECLUSTER_DISTANCE):
         count += 1
         clust = Cluster(group)
-        if clust.filter():  
-            sys.stdout.write(clust.__str__(count))
-            sys.stdout.write("\n")
+        # if clust.filter(): 
+            # sys.stdout.write(clust.__str__(count))
+            # sys.stdout.write("\n")
+        #if clust.filter():  clust.merge_pair_clusters()
 
 def cluster_generator(bamfile, max_dist):
     """Generator function that clusters \
@@ -236,7 +308,6 @@ def cluster_generator(bamfile, max_dist):
         prev = curr
 
     yield cluster
-
 
 
 def get_args():
@@ -275,22 +346,37 @@ class Usage(Exception):
 def main():
     args = get_args()
 
-    #set global clustering parameters
+    #   PARAMETERS: THIS WAY IS REALLY BAD. NEED TO REWORK PARAMETER SETTING!!
     global PRECLUSTER_DISTANCE
+    global PAIR_MERGE_DIST
+    global PAIR_MERGE_OLAP
+    global SPLIT_MERGE_DIST
+    global SPLIT_MERGE_OLAP
     global PAIR_CLUST_DIST
     global SPLIT_CLUST_DIST
 
-    PRECLUSTER_DISTANCE = 1300
-    PAIR_CLUST_DIST = 300
+    #   distance for bedtools cluster-like generic clustering
+    PRECLUSTER_DISTANCE = 1800
+
+    #   how close do reads need to be to be in same sub-cluster?
+    #   measured from rightmost-base if upstream of MEI (PR/SR)
+    #   leftmost-base if downstream of MEI (PL/SL)
+    PAIR_CLUST_DIST = 400
     SPLIT_CLUST_DIST = 7
 
-    global PAIR_MERGE_DIST
-
+    #   how close do concordant sub-clusters need to be to be merged?
+    #   what is the max overlap for merging pair clusters? 
+    #   (this effectively sets the max allowed TSDup / TSDel sizes)
+    #   NOTE: TSDel is far far less common than no TS alteration,
+    #   and TSDup is by FAR the most common)
     PAIR_MERGE_DIST = 800
-    # PAIR_MERGE_OLAP =
+    PAIR_MERGE_OLAP = 60
 
-    # SPLIT_MERGE_DIST = 
-    # SPLIT_MERGE_OLAP =
+    SPLIT_MERGE_DIST = 20
+    SPLIT_MERGE_OLAP = 60
+
+
+
 
     scan(args.i, args.S)
 
